06512bc7cd4a4adb54d58eb50867a28a55b8f585
EvgenyStefanenko <mixer@clarionlife.net>
2024-11-08 23:19:27 +1000

    Поменял одну фичу прибитую гвоздями

--- lib/shopCollection.plugin.php
+++ lib/shopCollection.plugin.php
@@ -4,7 +4,11 @@ class shopCollectionPlugin extends shopPlugin
 {
     public static function show($product_id, $feature_name, $feature_value)
     {
-        if ($feature_name == 'kollektsiya') {
+        $settings = wa('shop')->getPlugin('collection')->getSettings();
+        $collection_feature_id = $settings['collection_feature_id'];
+        $model = new shopFeatureModel();
+        $data = $model->getByField('id', $collection_feature_id);
+        if (isset($data['code']) && $data['code'] == $feature_name) {
             $url = "/webasyst/shop/?plugin=collection&action=go&id=" . $product_id;
             return "<a href=\"$url\">{$feature_value}</a>";
         }
